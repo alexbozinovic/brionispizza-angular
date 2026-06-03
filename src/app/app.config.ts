d1a2f3c3cdfc879a48a731b383cb37bf6f852023
alexbozinovic <alex.bozinovic@gmail.com>
2026-06-02 21:43:45 -0400

    installing firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,16 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "brionispizza-54eae", appId: "1:276474794528:web:f771665bacbd5c025fa245", storageBucket: "brionispizza-54eae.firebasestorage.app", apiKey: "AIzaSyA3vGmWkAZn7u0gfQ0woVye31Y1oaQr-CM", authDomain: "brionispizza-54eae.firebaseapp.com", messagingSenderId: "276474794528", measurementId: "G-0DYGG1YVRR", projectNumber: "276474794528", version: "2" })), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
