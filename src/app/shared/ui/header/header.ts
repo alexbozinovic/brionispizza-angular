2bfe452e9f7bcbaad48a996f58f5968a7843de18
alexbozinovic <alex.bozinovic@gmail.com>
2026-06-02 23:57:01 -0400

    header menu and logo are in place

--- src/app/shared/ui/header/header.ts
+++ src/app/shared/ui/header/header.ts
@@ -1,10 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterLink],
+  imports: [
+    CommonModule, 
+    RouterLink,
+    MatButtonModule
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
