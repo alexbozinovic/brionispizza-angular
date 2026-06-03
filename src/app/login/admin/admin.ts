acb4655f35550417e77346249f23d273d7c7267f
alexbozinovic <alex.bozinovic@gmail.com>
2026-06-02 23:09:24 -0400

    added app structure and login ui

---
+++ src/app/login/admin/admin.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin',
+  imports: [],
+  templateUrl: './admin.html',
+  styleUrls: ['./admin.scss'],
+})
+export class Admin {
+
+}
---
+++ src/app/login/login.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    RouterLink
+  ],
+  templateUrl: './login.html',
+  styleUrls: ['./login.scss'],
+})
+export class Login {
+
+}
